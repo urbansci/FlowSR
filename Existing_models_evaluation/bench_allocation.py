6ddc80b4bdf3fb9d6416b0aa27747a649f74d7be
张维昱 <weiyu_zhang@pku.edu.cn>
2024-09-14 09:41:53 +0800

    Upload existing models evaluation

---
+++ Existing_models_evaluation/bench_allocation.py
@@ -0,0 +1,146 @@
+# =================================================================================================================
+# Description: This script is used to evaluate the performance of existing models. The dataset to use is specifed by `dataset` and `level` variables. The models to evaluate are specified using `model_list`. The script will output the evaluation results in an Excel file.
+# Author: Weiyu Zhang, Hao Guo 
+# Date: Autumn 2024
+# =================================================================================================================
+from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
+from numpy import exp, round
+from lib_loaddata import *
+from matplotlib import pyplot as plt
+from scipy import optimize
+from math import sqrt
+import openpyxl
+
+dataset = 'US'  # ["england", "US", "JJJ","gd_commute","gd_mobility"]
+level = 'county'  # ["msoa", "mlad", "county", "state", "csa","subdistrict"]
+model_list = ["GM_Zipf", "GM_Pow","GM_Exp","RM","ERM","IO","OPS"]  # ["GM_Zipf", "GM_Pow","GM_Exp","RM","ERM","IO","OPS"]
+use_work_attr = True
+
+if dataset == 'gd_commute':
+    flow, dist, iores, iowork, attr = load_gd_commute_data(select_feat=['home_pop','work_pop'], level=level)
+elif dataset == 'gd_mobility':
+    flow, dist, iores, attr = load_gd_mobility_data(select_feat=['pop'], level=level)
+if dataset == 'england':
+    flow, dist, iores, iowork, attr = load_england_data_files(level=level,
+                select_feat=['respop', 'workpop'])
+elif dataset == 'US':
+    flow, dist, iores, iowork, attr = load_us_data_files(level=level,
+                select_feat=['respop', 'workpop'])
+elif dataset == 'JJJ':
+    flow, dist, iores, attr = load_jjj_data_files(level=level, select_feat=['pop_wan'])
+else:
+    raise NotImplementedError
+print(len(dist.keys()))
+
+reswb = openpyxl.Workbook()
+ws = reswb.active
+
+def prepare_data(ws, use_work_attr):
+    if dataset in ["england", "US","gd_commute"] and use_work_attr:
+        fid = 1  # 0: res 1: work
+        io = iowork
+    else:
+        fid = 0
+        io = iores
+    X = []
+    Y = []
+    ori_sep = [0]
+    ws.append(["ori", "dest", "dist", "io", "dpop", "opop", "vol"])
+    for o in dist.keys():
+        for d in flow[o].keys():
+            X.append([dist[o][d], io[o][d], attr[d][fid], attr[o][fid]])
+            Y.append(flow[o][d])
+            ws.append([o, d, dist[o][d], io[o][d], attr[d][fid], attr[o][fid], flow[o][d]])
+        ori_sep.append(ori_sep[-1]+len(flow[o].keys()))
+    Xarr = np.asarray(X)
+    Yarr = np.asarray(Y)
+    outflow = [sum(flow[o].values()) for o in dist.keys()]
+    return Xarr, Yarr, outflow, ori_sep
+
+# Prepare Data for running models
+Xarr, Yarr, outflow, ori_sep = prepare_data(ws, use_work_attr)
+print(len(Yarr))
+
+def allocation_law(dis, io, md, mo, param=None):
+    if type =="GM_Zipf":
+        p = md/dis
+    elif type == "GM_Pow":
+        p = md/(dis**param)
+    elif type == "GM_Exp":
+        p = md/exp(param*dis)
+    elif type == "RM":
+        p = md/((mo+io)*(mo+io+md))
+    elif type == "ERM":
+        p = ((mo+io+md)**param-(mo+io)**param)*(1+mo**param)/\
+            ((1+(mo+io)**param)*(1+(mo+io+md)**param))
+    elif type == "IO":
+        p = exp(param*io)-exp(param*(io+md))
+    elif type == "OPS":
+        p = md/(mo+io+md)
+    elif type == "custom":
+        p = (md ** 0.5127566392825407) / (dis ** 3 / md + mo)
+    else:
+        raise NotImplementedError
+    return p
+
+
+def mse_loss(param):
+    alloc = allocation_law(Xarr[:, 0], Xarr[:, 1], Xarr[:,2], Xarr[:,3], param)
+    for oid in range(len(dist.keys())):
+        stdfac = sum(alloc[ori_sep[oid]: ori_sep[oid+1]])
+        alloc[ori_sep[oid]: ori_sep[oid+1]] *= outflow[oid]/stdfac
+    return mean_squared_error(Yarr, alloc)
+
+
+def pred(param=None):
+    alloc = allocation_law(Xarr[:, 0], Xarr[:, 1], Xarr[:, 2], Xarr[:, 3], param)
+    for oid in range(len(dist.keys())):
+        stdfac = sum(alloc[ori_sep[oid]: ori_sep[oid + 1]])
+        alloc[ori_sep[oid]: ori_sep[oid + 1]] *= outflow[oid] / stdfac
+    return alloc
+
+mid = 0
+for type in model_list:
+    if type in ["GM_Pow", "GM_Exp", "IO","ERM"]:
+        if type == "IO":
+            init_param = [-0.001]  # Need Change
+            bound = [(-0.15, -0.0001)]  # IO: [(0.001, 1)]
+            met = "Nelder-Mead"  # ["BFGS","Nelder-Mead"]
+        else:
+            init_param = [1.0]  # Need Change
+            bound = None  # IO: [(0.001, 1)]
+            met = "Nelder-Mead"
+        #print(mse_loss(init_param))
+        res = optimize.minimize(mse_loss, np.asarray(init_param), method=met, options={"disp": True},
+                                bounds=bound)
+        print(res.x, res.fun)
+        Ypred = pred(res.x)
+    else:  # parameter-free model
+        Ypred = pred()
+
+    # Ypred = round(Ypred) # may be useful for scatterplot
+
+    rmse = sqrt(mean_squared_error(Yarr, Ypred))
+    mae = mean_absolute_error(Yarr, Ypred)
+    mape = mean_absolute_percentage_error(Yarr, Ypred)
+    cpc = 1-sum(np.abs(Ypred-Yarr))/(sum(Yarr)+sum(Ypred))
+    print(rmse, mae, mape, cpc)
+
+    # Output Predicted Flow
+    ws.cell(row=1, column=8+mid).value = type
+    for i in range(len(Ypred)):
+        ws.cell(row=2+i, column=8+mid).value = Ypred[i]
+
+    plt.figure(figsize=(6, 6))
+    plt.loglog(Yarr, Ypred, '.', markersize=1)
+    u = np.linspace(1, max(max(Yarr), max(Ypred)), 5000)
+    plt.loglog(u, u)
+    plt.xlim((0.8, 1.1*max(max(Yarr), max(Ypred))))
+    plt.ylim((0.8, 1.1*max(max(Yarr), max(Ypred))))
+    plt.xlabel('Truth')
+    plt.ylabel('Prediction')
+    plt.show()
+
+    mid += 1
+
+reswb.save("bench_"+ dataset + "_" + level + ".xlsx")
---
+++ Existing_models_evaluation/lib_loaddata.py
@@ -0,0 +1,219 @@
+# =================================================================================================================
+# Description: This file contains the functions to load the data for existing models' evaluation. The functions in this file will be called in the ./bench_allocation.py, depending on the dataset used for evaluation.
+# Author: Weiyu Zhang, Hao Guo 
+# Date: Autumn 2024
+# =================================================================================================================
+import openpyxl
+import numpy as np
+import pandas as pd
+import pickle
+from tqdm import tqdm
+
+def load_england_data_files(level='msoa', select_feat=None, modified_io=False):
+    # feat: dist, o, d
+    if level == 'msoa':
+        feat = {'area_km2': 3, 'respop': 4, 'employedpop': 5, 'workpop': 6, 'households':7,
+            'fb_pct': 8, 'deprived_pct': 9, 'nonwhite_pct': 10, 'bach_pct': 11, 'highsc_pct': 12}
+    elif level == 'mlad':
+        feat = {'respop': 4, 'workpop': 5}
+    flow_file = open(f"../data/England/England_{level}_census11_supp3.pkl", 'rb')
+    flow_dict = pickle.load(flow_file)
+    dist_file = open(f"../data/England/England_{level}_dist.pkl", 'rb')
+    dist_dict = pickle.load(dist_file)
+
+    units = list(dist_dict.keys())
+    geoid2row = dict()
+    attrdata = openpyxl.load_workbook(f"../data/England/England_{level}_census11_attr.xlsx")
+    attrtab = attrdata['attr']
+    for r in range(2, attrtab.max_row + 1):
+        geoid2row[int(attrtab.cell(r, 1).value[-6:])] = r
+    attr_dict = dict()
+    for o in units:
+        if select_feat is None:
+            attr = [attrtab.cell(geoid2row[o], c).value for c in feat.values()]
+        else:
+            attr = [attrtab.cell(geoid2row[o], feat[f]).value for f in select_feat]
+        attr_dict[o] = attr
+
+    if modified_io and level == 'mlad':
+        iores_file = open(f"../data/England/England_{level}_miores.pkl", 'rb')
+        iores_dict = pickle.load(iores_file)
+        iowork_file = open(f"../data/England/England_{level}_miowork.pkl", 'rb')
+        iowork_dict = pickle.load(iowork_file)
+    else:
+        iores_file = open(f"../data/England/England_{level}_iores.pkl", 'rb')
+        iores_dict = pickle.load(iores_file)
+        iowork_file = open(f"../data/England/England_{level}_iowork.pkl", 'rb')
+        iowork_dict = pickle.load(iowork_file)
+        if modified_io and level == 'msoa':
+            for o in iores_dict.keys():
+                for d in iores_dict[o].keys():
+                    iores_dict[o][d] += attrtab.cell(geoid2row[o], feat["respop"]).value
+                    iowork_dict[o][d] += attrtab.cell(geoid2row[o], feat["workpop"]).value
+    return flow_dict, dist_dict, iores_dict, iowork_dict, attr_dict
+
+def load_us_data_files(level='county', select_feat=None):
+    # feat: dist, o, d
+    if level == 'county':
+        feat = {'respop': 4, 'employedpop': 5, 'workpop': 6}
+    else:
+        raise NotImplementedError
+
+    flow_file = open(f"../data/US/us_acs15_{level}_flow.pkl", 'rb')
+    flow_dict = pickle.load(flow_file)
+    dist_file = open(f"../data/US/us_{level}_dist.pkl", 'rb')
+    dist_dict = pickle.load(dist_file)
+    iores_file = open(f"../data/US/us_{level}_iores.pkl", 'rb')
+    iowork_file = open(f"../data/US/us_{level}_iowork.pkl", 'rb')
+    iores_dict = pickle.load(iores_file)
+    iowork_dict = pickle.load(iowork_file)
+    msoa_units = list(dist_dict.keys())
+    geoid2row = dict()
+    attrdata = openpyxl.load_workbook(f"../data/US/us_acs15_{level}_attr.xlsx")
+    attrtab = attrdata['attr']
+    for r in range(2, attrtab.max_row + 1):
+        geoid2row[int(attrtab.cell(r, 1).value)] = r
+    attr_dict = dict()
+    for o in msoa_units:
+        if select_feat is None:
+            attr = [attrtab.cell(geoid2row[o], c).value for c in feat.values()]
+        else:
+            attr = [attrtab.cell(geoid2row[o], feat[f]).value for f in select_feat]
+        attr_dict[o] = attr
+    return flow_dict, dist_dict, iores_dict, iowork_dict, attr_dict
+
+def load_jjj_data_files(level='county', select_feat=None):
+    if level == 'county':
+        feat = {'area_km2': 8, 'pop_wan': 9, 'gdp_yi': 10}
+    else:
+        raise NotImplementedError
+    flow_file = open(f"../data/Jingjinji/JJJ_{level}_flow.pkl", 'rb')
+    flow_dict = pickle.load(flow_file)
+    dist_file = open(f"../data/Jingjinji/JJJ_{level}_dist.pkl", 'rb')
+    dist_dict = pickle.load(dist_file)
+    io_file = open(f"../data/Jingjinji/JJJ_{level}_io.pkl", 'rb')
+    io_dict = pickle.load(io_file)
+    units = list(dist_dict.keys())
+    geoid2row = dict()
+    attrdata = openpyxl.load_workbook(f"../data/Jingjinji/JJJ_{level}_attr.xlsx")
+    attrtab = attrdata['attr']
+    for r in range(2, attrtab.max_row + 1):
+        geoid2row[int(attrtab.cell(r, 5).value)] = r
+    attr_dict = dict()
+    for o in units:
+        if select_feat is None:
+            attr = [attrtab.cell(geoid2row[o], c).value for c in feat.values()]
+        else:
+            attr = [attrtab.cell(geoid2row[o], feat[f]).value for f in select_feat]
+        attr_dict[o] = attr
+    return flow_dict, dist_dict, io_dict, attr_dict
+
+def load_gd_commute_data(select_feat=None, level='subdistrict'):
+    import numpy as np
+    import pandas as pd
+    import pickle
+
+    # Load the raw data
+    flow_array = np.load(r"..\GD_data\Commuting_{}\gd_commute_flow_matrix_inter{}.npy".format(level,level))
+    with open(r"..\GD_data\Commuting_{}\gd_commute_ids_mapping_inter{}.pkl".format(level,level), "rb") as file:
+        id_dict = pickle.load(file)
+    dist_df = pd.read_csv(r"..\GD_data\Commuting_{}\gd_commute_dist_inter{}.csv".format(level,level))
+    attr_df = pd.read_csv(r"..\GD_data\Commuting_{}\gd_commute_attr_inter{}.csv".format(level,level))
+    oppo_df_res = pd.read_csv(r"..\GD_data\Commuting_{}\gd_commute_opportunity_inter{}_res.csv".format(level,level))
+    oppo_df_work = pd.read_csv(r"..\GD_data\Commuting_{}\gd_commute_opportunity_inter{}_work.csv".format(level,level))
+    # Check whether the selected features exist
+    if select_feat and not set(select_feat).issubset(attr_df.columns):
+        missing_feats = set(select_feat) - set(attr_df.columns)
+        raise ValueError(f"Features {missing_feats} not found in the dataset")
+
+    # Transform the DataFrame to a more efficient lookup structure
+    pivot_df = dist_df.pivot(index='o_id', columns='d_id', values='geodesic_dist')
+    dist_dict = pivot_df.to_dict(orient='index')
+    print("======> dist dict loaded")
+    pivot_df = oppo_df_res.pivot(index='o_id', columns='d_id', values='opportunity')
+    oppo_dict_res = pivot_df.to_dict(orient='index')
+    print("======> oppo dict res loaded")
+    pivot_df = oppo_df_work.pivot(index='o_id', columns='d_id', values='opportunity')
+    oppo_dict_work = pivot_df.to_dict(orient='index')
+    print("======> oppo dict work loaded")
+    # Initialize the dictionaries
+    flow_dict = {o_id: {} for o_id in id_dict.values()}
+    attr_dict = {}
+
+    for i in range(flow_array.shape[0]):
+        o_id = id_dict[i]
+
+        # Specify id column based on the level
+        if level == 'subdistrict':
+            id_col = 'street_num'
+        elif level == "county":
+            id_col = "county"
+        
+        if select_feat is None:
+            attr_dict[o_id] = attr_df[attr_df[id_col] == o_id].iloc[0].values[1:]
+        else:
+            attr_dict[o_id] = attr_df[attr_df[id_col] == o_id].iloc[0][select_feat].values
+        
+        # Drop the origin if there is no flow
+        if flow_array[i].sum() == 0:
+            flow_dict.pop(o_id)
+            continue
+        for j in range(flow_array.shape[1]):
+            d_id = id_dict[j]
+            if i != j and flow_array[i,j]!=0:  # Exclude the flow from the origin to itself
+                flow_dict[o_id][d_id] = flow_array[i, j]
+    return flow_dict, dist_dict, oppo_dict_res, oppo_dict_work, attr_dict
+
+def load_gd_mobility_data(select_feat=None, level='subdistrict'):
+    import numpy as np
+    import pandas as pd
+    import pickle
+
+    # Load the raw data
+    flow_array = np.load(r"..\GD_data\Mobility_{}\gd_mobility_flow_matrix_inter{}.npy".format(level,level))
+    with open(r"..\GD_data\Mobility_{}\gd_mobility_ids_mapping_inter{}.pkl".format(level,level), "rb") as file:
+        id_dict = pickle.load(file)
+    dist_df = pd.read_csv(r"..\GD_data\Mobility_{}\gd_mobility_dist_inter{}.csv".format(level,level))
+    attr_df = pd.read_csv(r"..\GD_data\Mobility_{}\gd_mobility_attr_inter{}.csv".format(level,level))
+    oppo_df = pd.read_csv(r"..\GD_data\Mobility_{}\gd_mobility_opportunity_inter{}.csv".format(level,level))
+    
+    # Check whether the selected features exist
+    if select_feat and not set(select_feat).issubset(attr_df.columns):
+        missing_feats = set(select_feat) - set(attr_df.columns)
+        raise ValueError(f"Features {missing_feats} not found in the dataset")
+
+    # Transform the DataFrame to a more efficient lookup structure
+    pivot_df = dist_df.pivot(index='o_id', columns='d_id', values='geodesic_dist')
+    dist_dict = pivot_df.to_dict(orient='index')
+    print("======> dist dict loaded")
+    pivot_df = oppo_df.pivot(index='o_id', columns='d_id', values='opportunity')
+    oppo_dict = pivot_df.to_dict(orient='index')
+    print("======> oppo dict loaded")
+    
+    # Initialize the dictionaries
+    flow_dict = {o_id: {} for o_id in id_dict.values()}
+    attr_dict = {}
+
+    for i in range(flow_array.shape[0]):
+        o_id = id_dict[i]
+        
+        # Specify id column based on the level
+        if level == 'subdistrict':
+            id_col = 'street_num'
+        elif level == "county":
+            id_col = "county"
+        
+        if select_feat is None:
+            attr_dict[o_id] = attr_df[attr_df[id_col] == o_id].iloc[0].values[1:]
+        else:
+            attr_dict[o_id] = attr_df[attr_df[id_col] == o_id].iloc[0][select_feat].values
+        
+        if flow_array[i].sum() == 0:
+            flow_dict.pop(o_id)
+            continue
+        for j in range(flow_array.shape[1]):
+            d_id = id_dict[j]
+            if i != j and flow_array[i,j]!=0:  # Exclude the flow from the origin to itself
+                flow_dict[o_id][d_id] = flow_array[i, j]
+                
+    return flow_dict, dist_dict, oppo_dict, attr_dict
